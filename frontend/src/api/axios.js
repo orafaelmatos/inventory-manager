a9fd04cc2eac14cd233a136928beb1d11480473a
Rafael Matos <rafaelmatos.dev1@gmail.com>
2025-07-18 14:20:37 -0300

    adjust api url base

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://inventory-manager-t37m.onrender.com",
+  baseURL: "https://inventory-manager-t37m.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {

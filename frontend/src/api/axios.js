5a62fbbacceb93e5d2f685581ffd46d5ab41032e
Rafael Matos <rafaelmatos.dev1@gmail.com>
2025-07-18 14:02:21 -0300

    config cors

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: "https://inventory-manager-t37m.onrender.com",
 });
 
 api.interceptors.request.use((config) => {

744a529ee757d29a6bf3885b95839ea1fdc1d70d
Rafael Matos <rafaelmatos.dev1@gmail.com>
2025-07-18 14:36:53 -0300

    set cors

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -36,7 +36,8 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://inventory-manager-88v0hoe62-rafael-matos-projects.vercel.app",
-    "inventory-manager-zeta-sooty.vercel.app"
+    "https://inventory-manager-zeta-sooty.vercel.app",
+    "https://inventory-manager-66szi1qtd-rafael-matos-projects.vercel.app"
 
 ]
 
